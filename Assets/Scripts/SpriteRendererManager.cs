05c17fea125b6a4481b476d3be93066ca62dc49e
Coedgy <fugi501@gmail.com>
2021-03-16 16:47:01 +0200

    Initial commit

---
+++ Assets/Scripts/SpriteRendererManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRendererManager : MonoBehaviour
+{
+    SpriteRenderer[] rend;
+    MeshRenderer[] meshrend;
+
+    void Start()
+    {
+        rend = FindObjectsOfType<SpriteRenderer>();
+        meshrend = FindObjectsOfType<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        foreach (SpriteRenderer rend in rend)
+        {
+            rend.sortingOrder = (int)(rend.transform.position.y * -100);
+        }
+        foreach (MeshRenderer meshrend in meshrend)
+        {
+            meshrend.sortingOrder = (int)(meshrend.transform.position.y * -100);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ROTMG-Remake
+ 
