88222ea066ef3c647327b657b4f467ad91306e1e
Coedgy <fugi501@gmail.com>
2021-03-24 02:35:59 +0200

    Updated Slot.cs

--- Assets/Scripts/Inventory/Slot.cs
+++ Assets/Scripts/Inventory/Slot.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Slot : MonoBehaviour
 {
+    
+    public bool isEquipmentSlot;
+    public int amount;
+
     // Start is called before the first frame update
     void Start()
     {
