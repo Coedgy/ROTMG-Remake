14845c17beed1a91dafcc97314910ef276c3757a
Coedgy <fugi501@gmail.com>
2021-03-26 01:08:11 +0200

    Created an item database system
    Added item scripts
    Added itemDatabase scripts
    Added itemDatabaseManager

--- Assets/Scripts/Item related/Item.cs
+++ Assets/Scripts/Item related/Item.cs
@@ -1,14 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+[CreateAssetMenu(fileName = "New Item", menuName = "Assets/Item")]
 public class Item : ScriptableObject
 {
-
+    public int ID;
     public new string name;
+    [TextArea]
     public string description;
 
     public Sprite artwork;
 
+    public bool stackable = false;
+    public int maxStack = 1;
 }
---
+++ Assets/Scripts/ItemDatabaseManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabaseManager : MonoBehaviour
+{
+    public ItemDatabase database;
+
+    static ItemDatabaseManager script;
+
+    private void Awake()
+    {
+        if (script == null)
+        {
+            script = this;
+        }
+    }
+
+    private void Start()
+    {
+        VerifyItemsID();
+    }
+
+    public static void VerifyItemsID()
+    {
+        int idSumTarget = 0;
+        int idSum = 0;
+
+        foreach (Item item in script.database.allItems)
+        {
+            idSum += item.ID;
+        }
+
+        idSumTarget = (script.database.allItems.Count * (script.database.allItems.Count + 1) / 2);
+
+        if (idSum != idSumTarget)
+        {
+            throw new System.Exception("ItemDatabase ID verifying failed, idSUM was '" + idSum + "', and it should be '" + idSumTarget + "'");
+        }
+    }
+
+    public static Item GetItemByID(int ID)
+    {
+        foreach (Item item in script.database.allItems)
+        {
+            if (item.ID == ID)
+            {
+                return item;
+            }
+        }
+
+        Debug.LogError("Item by ID '" + ID + "' was not found");
+        return null;
+    }
+}
