6002e2f42c752a6f3f76a2c5f4bf3b18770afb6c
Coedgy <fugi501@gmail.com>
2021-03-25 23:02:12 +0200

    Added potion sprites & created Item ScriptableObject

---
+++ Assets/Scripts/Item related/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+public class Item : ScriptableObject
+{
+
+    public new string name;
+    public string description;
+
+    public Sprite artwork;
+
+}
