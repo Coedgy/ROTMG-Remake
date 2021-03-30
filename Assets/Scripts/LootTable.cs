37652bca9bdb1d2677a5b610088f4fd001740d1f
Coedgy <fugi501@gmail.com>
2021-03-30 13:34:37 +0300

    created lootTable scripts

---
+++ Assets/Scripts/LootTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New LootTable", menuName = "Loot Table")]
+public class LootTable : ScriptableObject
+{
+    public List<LootTableEntry> lootList;
+}
+
+[Serializable]
+public struct LootTableEntry
+{
+    public int itemID;
+    public int amount;
+    public float dropPoints;
+}
