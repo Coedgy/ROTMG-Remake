3e96591945871993412b93c6cebc1409be8748cb
Coedgy <fugi501@gmail.com>
2021-04-12 00:47:17 +0300

    Characters menu

---
+++ Assets/Scripts/Main menu related/CharactersList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class CharactersList : MonoBehaviour
+{
+    public GameObject slotTemplate;
+    public GameObject newSlotButton;
+
+    public void CharacterButtonAction(GameObject slot)
+    {
+        Debug.Log("CharacterAction: " + slot);
+    }
+    
+    public void RemoveButtonAction(GameObject slot)
+    {
+        Destroy(slot);
+    }
+
+    public void NewCharacterAction()
+    {
+        GameObject newSlot = Instantiate(slotTemplate, transform);
+        newSlot.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "Knight " + Random.Range(0,21);
+        newSlotButton.transform.SetAsLastSibling();
+    }
+}
