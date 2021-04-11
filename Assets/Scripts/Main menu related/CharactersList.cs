92af2d24f903fa13aae1f7e87d37806ada38fd13
Coedgy <fugi501@gmail.com>
2021-04-12 01:14:27 +0300

    Characters menu updates
    Moved the character slot to a prefab form

--- Assets/Scripts/Main menu related/CharactersList.cs
+++ Assets/Scripts/Main menu related/CharactersList.cs
@@ -12,17 +12,24 @@ public class CharactersList : MonoBehaviour
     public void CharacterButtonAction(GameObject slot)
     {
         Debug.Log("CharacterAction: " + slot);
+        
+        //TODO go to gameplay scene with the chosen character
     }
     
     public void RemoveButtonAction(GameObject slot)
     {
         Destroy(slot);
+        //TODO Remove character from database and refresh the character screen
     }
 
     public void NewCharacterAction()
     {
         GameObject newSlot = Instantiate(slotTemplate, transform);
         newSlot.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "Knight " + Random.Range(0,21);
+        newSlot.GetComponent<Button>().onClick.AddListener(() => CharacterButtonAction(newSlot));
+        newSlot.transform.GetChild(4).GetComponent<Button>().onClick.AddListener(() => RemoveButtonAction(newSlot));
         newSlotButton.transform.SetAsLastSibling();
+        
+        //TODO go to character creation screen
     }
 }
